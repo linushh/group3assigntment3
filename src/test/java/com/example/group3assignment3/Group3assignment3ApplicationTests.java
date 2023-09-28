9162ad493700e73f1f6dfe3ce57bdcb6c0909e0d
Linus Håkansson <Linus.Hakansson@yh.nackademin.se>
2023-09-28 12:05:40 +0200

    first commit

---
+++ src/test/java/com/example/group3assignment3/Group3assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group3assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3assignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
