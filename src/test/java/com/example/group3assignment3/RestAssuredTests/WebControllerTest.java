9028865571e78c56c32fa257d05fec2be146998e
MaxHalling <hallingmaax@gmail.com>
2023-09-28 12:29:47 +0200

    added hello method test

---
+++ src/test/java/com/example/group3assignment3/RestAssuredTests/WebControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.group3assignment3.RestAssuredTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+class WebControllerTest {
+
+    @Test
+    public void helloWorldReturnsCorrectMessage() {
+        RestAssured.baseURI = "http://localhost:8080";
+        given()
+                .when()
+                .get("/hello/Max")
+                .then()
+                .statusCode(200)
+                .body("respons", equalTo("Hello, Max"));
+    }
+}
