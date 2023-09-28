8377e1ef2eea8939124cae57795ebbaa7a16e399
MaxHalling <hallingmaax@gmail.com>
2023-09-28 12:21:15 +0200

    added hello method

--- src/main/java/com/example/group3assignment3/Controllers/HelloWorldController.java
+++
@@ -1,4 +0,0 @@
-package com.example.group3assignment3.Controllers;
-
-public class HelloWorldController {
-}
---
+++ src/main/java/com/example/group3assignment3/Controllers/WebController.java
@@ -0,0 +1,32 @@
+package com.example.group3assignment3.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    /* I denna metod tas ett namn in som en PathVariable och metoden returnerar
+    då en hälsning och det namn som användaren angett
+    */
+
+    @GetMapping("/hello/{name}")
+    public Respons helloName(@PathVariable String name) {
+        return new Respons(name);
+    }
+
+    public class Respons {
+
+        private String respons;
+
+        public Respons(String name) {
+            this.respons = "Hello, " + name;
+        }
+
+        public String getRespons() {
+            return respons;
+        }
+    }
+
+}
